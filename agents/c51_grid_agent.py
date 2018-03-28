f647dc6f44f3b0c5c0e972bdc30cac662754d860
Jonathan <jonsecu0001@hotmail.com>
2018-03-28 14:42:30 +0200

    Antes de modificar c51.

--- agents/c51_grid_agent.py
+++ agents/c51_grid_agent.py
@@ -46,8 +46,8 @@ class C51GridAgent:
         self.misc = {'final_state': None}
         self.prev_misc = self.misc
 
-        self.action_size = 4
-        self.img_rows, self.img_cols = 3, 3
+        self.action_size = 2
+        self.img_rows, self.img_cols = 1, 5
         self.img_channels = 2
 
         # C51
@@ -108,6 +108,7 @@ class C51GridAgent:
             # Epsilon Greedy
             self.action_idx = input("action")
             # self.action_idx = self.agent.get_action(self.s_t)
+
             self.a_t[self.action_idx] = 1
             self.obs, self.r_t, self.done, self.misc = self.env.step(self.action_idx)
             self.is_terminated = self.done
