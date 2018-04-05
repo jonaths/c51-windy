318c9d2a81931c5239c537cec787c277592d45ba
Jonathan <jonsecu0001@hotmail.com>
2018-04-05 14:47:41 +0200

    Modelo entrenado. Desaparecio el ruido al disminuir el timeout del ambiente.

--- agents/c51_grid_agent.py
+++ agents/c51_grid_agent.py
@@ -113,20 +113,20 @@ class C51GridAgent:
             self.a_t = np.zeros([self.action_size])
 
             # sleep(0.1)
-            # print("st:", self.s_t1)
-            # self.env.render()
-            # self.agent.plot_histogram(self.s_t1)
+            print("st:", self.s_t1)
+            self.env.render()
+            self.agent.plot_histogram(self.s_t1)
 
-            # self.action_idx = input("action")
-            self.action_idx = self.agent.get_action(self.s_t)
+            self.action_idx = input("action")
+            # self.action_idx = self.agent.get_action(self.s_t)
             # self.action_idx = self.agent.get_action(self.s_t, current_budget)
 
             self.a_t[self.action_idx] = 1
             self.obs, self.r_t, self.done, self.misc = self.env.step(self.action_idx)
             self.is_terminated = self.done
 
-            print("misc")
-            print(self.misc)
+            # print("misc")
+            # print(self.misc)
 
             if self.is_terminated:
 
@@ -177,9 +177,9 @@ class C51GridAgent:
             self.t += 1
 
             # save progress every 10000 iterations
-            # if self.t % 1000 == 0:
-            #     print("Now we save model")
-            #     self.agent.model.save_weights("models/c51_ddqn.h5", overwrite=True)
+            if self.t % 1000 == 0:
+                print("Now we save model")
+                self.agent.model.save_weights("models/c51_ddqn.h5", overwrite=True)
 
             # print info
 
